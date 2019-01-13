9817f10a7591f9140eff24af18e6e20355c72a30
Armin <Armin.Bart@xpecto.com>
2019-01-13 15:02:25 +0100

    New Function getExtension()

--- src/de/bartmail/moviebutler/main/Util.java
+++ src/de/bartmail/moviebutler/main/Util.java
@@ -10,4 +10,25 @@ public class Util
 
 		return false;
 	}
+
+	// Stolen function that is clamed to be tested
+	public static String getExtension(String fileName)
+	{
+		char ch;
+		int len;
+
+		if (fileName == null ||
+				(len = fileName.length())==0 ||
+				(ch = fileName.charAt(len-1))=='/' || ch=='\\' || //in the case of a directory
+				 ch=='.') //in the case of . or ..
+			return "";
+
+		int dotInd = fileName.lastIndexOf('.');
+		int sepInd = Math.max(fileName.lastIndexOf('/'), fileName.lastIndexOf('\\'));
+
+		if (dotInd <= sepInd)
+			return "";
+		else
+			return fileName.substring(dotInd+1).toLowerCase();
+	}
 }
